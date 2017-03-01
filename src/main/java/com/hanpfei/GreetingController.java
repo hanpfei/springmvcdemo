95286a2e17e445cb45277dc826c565005e642b7b
Han Pengfei <hanpfei@gmail.com>
2017-03-01 16:06:34 +0800

    Add kafka producer example.
    Signed-off-by: Han Pengfei <hanpfei@gmail.com>

--- src/main/java/com/hanpfei/GreetingController.java
+++ src/main/java/com/hanpfei/GreetingController.java
@@ -1,10 +1,11 @@
 package com.hanpfei;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Properties;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -12,14 +13,37 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping(value="/", method= RequestMethod.GET)
-    public String index() {
+    @RequestMapping("/")
+    public @ResponseBody String index() {
         return "index";
     }
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                String.format(template, name));
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    private void sendMessage(String sentMsg) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        producer.send(new ProducerRecord<String, String>("test", sentMsg, sentMsg));
+
+        producer.close();
+    }
+
+    @RequestMapping("/sendMsg")
+    public Greeting message(@RequestParam(value="msg", defaultValue="World") String msg) {
+        String sentMsg = String.format(template, msg);
+        sendMessage(sentMsg);
+        return new Greeting(counter.incrementAndGet(), sentMsg);
     }
 }
